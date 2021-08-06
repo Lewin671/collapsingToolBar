ebf44acd3a426b6a285d2e5e8cecaaf3942e71b4
Lewin671 <2596736318@qq.com>
2021-08-07 05:16:46 +0800

    feat: add title

--- app/src/main/java/com/example/collapsingappbartest/MainActivity.kt
+++ app/src/main/java/com/example/collapsingappbartest/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.collapsingappbartest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolBar = findViewById<Toolbar>(R.id.toolbar)
+        val collapsingToolBarLayout =
+            findViewById<CollapsingToolbarLayout>(R.id.collapsingToolBarLayout)
+        setSupportActionBar(toolBar)
+        collapsingToolBarLayout.apply {
+            setCollapsedTitleTextColor(Color.WHITE)
+            setExpandedTitleColor(Color.WHITE)
+
+        }
     }
 }
